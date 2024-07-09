af8e125487427e353f265e0ac7ae82978d7db40f
o0Mink0o <141802449+o0Mink0o@users.noreply.github.com>
2024-07-09 10:18:23 +0700

    comP

--- q2.ts
+++ q2.ts
@@ -3,9 +3,27 @@ import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
-  /* Your code here */
-};
+  try {
+    const resTodo = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+    const Todo = resTodo.data;
+    const userId = Todo.userId;
 
+
+    const resUser = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const user = resUser.data;
+    
+    return{
+      owner : user.name,
+      title : Todo.title,
+      completed : Todo.completed
+    };
+  
+
+  } catch (error) {
+    return "INVALID TODO ID";
+
+};
+}
 //test case
 const input1 = 15;
 const input2 = 60;
